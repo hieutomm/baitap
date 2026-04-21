aecd07ef8484b3a74aecbff608e990f61b5ccbc7
tom <minhhieu30701@gmail.com>
2026-04-21 23:13:54 +0700

    edit

--- fetchAPI.js
+++ fetchAPI.js
@@ -2,7 +2,6 @@
 //   try {
 //     let res = await fetch("https://jsonplaceholder.typicode.com/users");
 //     let data = await res.json();
-//     var html = "";
 //     data.forEach(function (user) {
 //       console.log("Name:", user.name);
 //       console.log("City:", user.address.city);
